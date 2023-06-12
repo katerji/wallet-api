7f1294ebac527b034abbce251eeeab13a241a3b9
katerji <katerji.abedalaziz@gmail.com>
2023-06-12 18:49:33 +0300

    Chore: init db

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/katerji/UserAuthKit/controller"
+	"github.com/katerji/UserAuthKit/db"
 )
 
 func main() {
+
+	initDB()
+
 	router := gin.Default()
 
 	router.GET(controller.LandingPath, controller.LandingController)
 
 	router.POST(controller.RegisterPath, controller.RegisterController)
 
+}
+
+func initDB() {
+	client := db.GetDatabaseInstance()
+	err := client.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
