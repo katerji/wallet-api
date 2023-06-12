9518ad8a4a81e86d7c77889b8141cd84127cd89b
katerji <katerji.abedalaziz@gmail.com>
2023-06-12 18:45:57 +0300

    Chore: create db client

--- db/client.go
+++ db/client.go
@@ -1,1 +1,42 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+)
+
+type Client struct {
+	*sql.DB
+}
+
+var client *Client
+
+func GetDatabaseInstance() *Client {
+	if client != nil {
+		return client
+	}
+	db := createClient()
+	client = &Client{db}
+	return client
+}
+
+func createClient() *sql.DB {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_DATABASE")
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	return db
+}
