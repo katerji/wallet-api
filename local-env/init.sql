78d6c673c6603a8640066eccc0dd67da4d3ae88b
katerji <katerji.abedalaziz@gmail.com>
2023-06-19 15:48:24 +0300

    Feat: implement cron job that fetches token info every 5 minutes and inserts in mysql

--- local-env/init.sql
+++ local-env/init.sql
@@ -3,22 +3,27 @@ CREATE DATABASE app;
 USE app;
 CREATE TABLE `user`
 (
-    `id`       int(11) NOT NULL AUTO_INCREMENT,
-    `email`    varchar(255) NOT NULL,
-    `username` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
+    `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `email`      varchar(255) NOT NULL,
+    `username`   varchar(255) NOT NULL,
+    `password`   varchar(255) NOT NULL,
+    `created_on` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_on` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY `email` (`email`),
     UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `token`
 (
-    `id`                      varchar(255) NOT NULL,
-    `name`                    varchar(255) NOT NULL,
-    `ticker`                  varchar(255) NOT NULL,
-    `symbol`                  varchar(255) NOT NULL,
-    `24hrs_change_percentage` float DEFAULT NULL,
+    `id`                varchar(255) NOT NULL,
+    `name`              varchar(255) NOT NULL,
+    `ticker`            varchar(255) NOT NULL,
+    `symbol`            varchar(255) NOT NULL,
+    `price`             float        NOT NULL,
+    `change_percentage` float                 DEFAULT NULL,
+    `created_on`        timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_on`        timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY                       `name` (`name`),
-    KEY                       `ticker` (`ticker`)
+    KEY                 `name` (`name`),
+    KEY                 `ticker` (`ticker`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
